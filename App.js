cfb2bf8399f2311db46074b5ce2362b7c44968c7
6210110322 <6210110322@psu.ac.th>
2021-07-17 00:51:33 +0700

    part 6

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import ZipCodeScreen from './screen/ZipCodeScreen';
+import { StyleSheet, View } from 'react-native';
 import WeatherScreen from './screen/WeatherScreen';
-
-const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={ZipCodeScreen} />
-        <Stack.Screen name="Weather" component={WeatherScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <WeatherScreen />
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- screen/WeatherScreen.js
+++ screen/WeatherScreen.js
@@ -1,11 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { View } from 'react-native';
-import Weather from '../components/Weather';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function WeatherScreen({route}){
-    return(
-        <View>
-            <Weather zipCode={route.params.zipCode}/>
-        </View>
-    )
+export default function WeatherScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
