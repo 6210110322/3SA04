86c44f4f63d4611fbbf957bb2c8047e8e4191c8a
6210110322 <6210110322@psu.ac.th>
2021-07-17 01:10:39 +0700

    part 6 loading

--- screen/WeatherScreen.js
+++ screen/WeatherScreen.js
@@ -1,9 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
-export default function WeatherScreen() {
-  return (
+export default function WeatherScreen(){
+    const [weatherData, setWeatherData] = useState(null);
+    const [loaded, setLoaded] = useState(true);
+
+    async function fetchWeatherData(cityName){
+        setLoaded(false);
+        const API = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=31464faa5dc561c0fe0777b7d68a5230`
+
+        try{
+            const response = await fetch(API);
+            if(response.status == 200){
+                const data = await response.json();
+                setWeatherData(data);
+            }else{
+                setWeatherData(null);
+            }
+            setLoaded(true);
+
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchWeatherData('Songkhla');
+        console.log(weatherData);
+    }, [])
+
+    if(!loaded){
+        return(
+            <View style={styles.container}>
+                <ActivityIndicator color='gray' size={36} />
+            </View>
+        )
+    }
+    
+    return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
